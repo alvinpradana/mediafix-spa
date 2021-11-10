9986056966bd82bf668c6e8b86f5d7ef70c6929e
Alvin Novian Perdana <alvinpradana14@gmail.com>
2021-11-10 08:08:16 +0700

    Add a user at user seeder

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -16,10 +16,20 @@ class UsersSeeder extends Seeder
     {
         collect([
             [
-                'name' => 'Admin Test',
+                'name' => 'Test',
+                'username' => 'test123',
+                'user_added' => 'test123',
+                'email' => 'test@email.com',
+                'password' => 'password',
+                'phone' => '081234567890',
+                'workshop' => 'Yogyakarta',
+                'email_verified_at' => now(),
+            ],
+            [
+                'name' => 'Admin',
                 'username' => 'admin2021',
                 'user_added' => 'admin2021',
-                'email' => 'test@email.com',
+                'email' => 'admin@email.com',
                 'password' => 'password',
                 'phone' => '081328944121',
                 'workshop' => 'Yogyakarta',
